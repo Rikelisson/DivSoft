c426225e71b778d5637403040c3f3e48600f1f4d
Rikelisson <rikelisson99@hotmail.com>
2020-04-20 17:20:30 -0300

    DiviSoftV1.0.0.5

--- DivSoft/src/main/java/com/Divsoft/Entidade/Funcionario.java
+++ DivSoft/src/main/java/com/Divsoft/Entidade/Funcionario.java
@@ -25,9 +25,10 @@ public class Funcionario implements Serializable{
 	@Length(max=60 ,min = 10, message = "O nome deve ter entre 10 e 60 caracteres")
 	private String nome;
 	
-	@Column(name = "login",length = 80, nullable = true)
-	@Email(message = "Informe um E-mail v·lido")
-	private String Login;
+	@Column(name = "login",length = 80, nullable = false)
+	@NotBlank(message = "Campo Obrigatorio")
+	@Email(message = "Informe um E-mail v√°lido")
+	private String login;
 	
 	@Column(name = "senha", nullable = false)
 	private  String senha;
@@ -46,13 +47,13 @@ public class Funcionario implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getLogin() {
-		return Login;
+		return login;
 	}
 	public void setLogin(String login) {
-		Login = login;
+		this.login = login;
 	}
-	
 	public String getSenha() {
 		return senha;
 	}
@@ -61,21 +62,22 @@ public class Funcionario implements Serializable{
 	}
 	public Funcionario() {}
 	
+	
+	
 	public Funcionario(Long id, String nome, String login, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		Login = login;
+		this.login = login;
 		this.senha = senha;
 	}
-	
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Login == null) ? 0 : Login.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		return result;
@@ -89,15 +91,15 @@ public class Funcionario implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Funcionario other = (Funcionario) obj;
-		if (Login == null) {
-			if (other.Login != null)
-				return false;
-		} else if (!Login.equals(other.Login))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
 			return false;
 		if (nome == null) {
 			if (other.nome != null)
@@ -111,6 +113,10 @@ public class Funcionario implements Serializable{
 			return false;
 		return true;
 	}
+	
+	
+	
+	
 	
 	
 	
